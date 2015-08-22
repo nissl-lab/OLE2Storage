5926cf5c6177ba593e67ebee3c813fa0a8aa24d3
Tony Qu <tonyqus@163.com>
2015-08-22 23:58:37 +0800

    first checkin

---
+++ NStorage/OldExcelFormatException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NStorage
+{
+    public class OldExcelFormatException:Exception
+    {
+        public OldExcelFormatException(String s)
+            : base(s)
+        { }
+
+    }
+}
---
+++ NStorage/Utility/AssertFailedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NStorage.Utility
+{
+    internal class AssertFailedException : ApplicationException
+    {
+        public AssertFailedException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
